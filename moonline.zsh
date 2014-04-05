4c0418edf7be323991c2aabe244efc8752968f88
kagamilove0707 <kagamilove0707@gmail.com>
2014-04-05 22:10:06 +0900

    shell variable setting in moonline.zsh

--- moonline.zsh
+++ moonline.zsh
@@ -1,43 +1,54 @@
-# global variable names (types)
-# " description
-# 
-# MOONLINE_HOME (scalar)
-# " a path to directory installing moonlight
-#
-# MOONLINE_COMPONENTS (association)
-# " a hash of component
-#
-# MOONLINE_SEPRATOR (array) 
-# MOONLINE_SUBSEPARATOR (array)
-# " strings using as separator.
-# " first item is a left separator
-# " second item is a right separator
-#
-# MOONLINE_LEFT1..4 (array)
-# MOONLINE_RIGHT1..4 (array)
-# MOONLINE_PROMPT (scalar)
-# " displaying component(s)
-#
-# MOONLINE_COLORSCHEME_PATH (array)
-# MOONLINE_COLORSCHEME (scalar)
-# MOONLINE_COLOR_LEFT_LINE (scalar)
-# MOONLINE_COLOR_LEFT{1..$MOONLINE_LEFT_LINE}}_{1..4} (array)
-# MOONLINE_COLOR_RIGHT{1..4} (array)
-# MOONLINE_COLOR_PROMPT (array)
+# set shell varibles
 
+# moonline.zsh installed directory's path
 MOONLINE_HOME="$(cd "$(dirname ${0})"; pwd)"
 
-# load default settings
-for default_file ($MOONLINE_HOME/default/*.zsh); do
-  source $default_file
-done
+# moonline.zsh prompt's separatos (left right)
+MOONLINE_SEPARATOR=('' '')
+MOONLINE_SUBSEPARATOR=('|' '|')
 
-fpath+=(
-  "$MOONLINE_HOME/functions"
+# moonline.zsh components
+typeset -A MOONLINE_COMPONENTS
+# load components
+(){
+  local component_file
+  for component_file ($MOONLINE_HOME/components/*.zsh); do
+    source $component_file
+  done
+}
+
+# moonline.zsh default prompt
+# number of left prompt line
+MOONLINE_LEFT_LINE=1
+# left prompt groups (max 4)
+MOONLINE_LEFT1_1=(username)
+MOONLINE_LEFT1_2=(current_path)
+# right prompt groups (max 4)
+MOONLINE_RIGHT1=(time)
+
+# moonline.zsh find colorscheme using this pathes
+MOONLINE_COLORSCHEME_PATH=''
+typeset -U MOONLINE_COLORSCHEME_PATH moonline_colorscheme_path
+moonline_colorscheme_path=(
+  $MOONLINE_HOME/colorschemes
 )
-autoload -Uz moonline-initialize
-autoload -Uz moonline-version
 
+# colorscheme using moonline.zsh
+MOONLINE_COLORSCHEME="default"
+
+# set fpath for autoload
+fpath+=(
+  "$MOONLINE_HOME/subcommands"
+)
+# define subcommands
+(){
+  local subcommand
+  for subcommand ($MOONLINE_HOME/subcommands); do
+    autoload -Uz $subcommand
+  done
+}
+
+# implementation of moonline command
 moonline() {
   moonline-${1} ${*[2,-1]}
 }
