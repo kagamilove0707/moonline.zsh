d76e7d8d32421ba01302ad29f6c96d4e225f7ef6
kagamilove0707 <kagamilove0707@gmail.com>
2014-04-05 22:09:35 +0900

    fix some directory layout

--- default/colorscheme.zsh
+++
@@ -1,7 +0,0 @@
-MOONLINE_COLORSCHEME_PATH=''
-typeset -T MOONLINE_COLORSCHEME_PATH moonline_colorscheme_path
-typeset -U moonline_colorscheme_path
-moonline_colorscheme_path+=(
-  "$MOONLINE_HOME/colorscheme"
-)
-MOONLINE_COLORSCHEME="default"
